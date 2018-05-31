56bb0a1c5d059b1373641a2edd5825568be5d7b7
afrozMT <afroz.alam@mindtickle.com>
2018-06-01 01:51:57 +0530

    adding documentation

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,35 +1,35 @@
-import { configure, setAddon,addDecorator} from '@storybook/react';
-import { setOptions } from '@storybook/addon-options';
-
+import { configure, setAddon, addDecorator, setKindOrdering } from "@storybook/react";
+import { setOptions } from "@storybook/addon-options";
 setOptions({
-  name: 'MT React Components',
-  url: 'https://github.com/Mindtickle/mt-ui-components',
+  name: "MT React Components",
+  url: "https://github.com/Mindtickle/mt-ui-components",
   goFullScreen: false,
   showLeftPanel: true,
   showDownPanel: true,
   showSearchBox: false,
-  downPanelInRight: true,
-  sortStoriesByKind: true,
+  downPanelInRight: true
 });
 
-addDecorator((story) => {
+addDecorator(story => {
   return story();
 });
 
 addDecorator(story => (
   <div>
-    <div style={{ padding: 10}}>
-      {story()}
-    </div>
+    <div style={{ padding: 10 }}>{story()}</div>
   </div>
 ));
 
-
-
-const req = require.context('../src/components', true, /.stories.js$/);
+const req = require.context("../src/components", true, /.stories.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  require("../src/components/Introduction/introduction.stories");
+  req.keys().forEach(filename => {
+    if (filename.indexOf("introduction.stories") > -1) {
+      return;
+    }
+    req(filename);
+  });
 }
 
 configure(loadStories, module);
