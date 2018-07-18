02b431a201a2f1e589cd0da3d92048185a394b96
MindTickle <mindtickle@Sandeeps-MacBook-Pro.local>
2018-07-18 16:39:18 +0530

    add type in modl for size

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -4,14 +4,25 @@ import AntModal from 'antd/lib/modal';
 import 'antd/lib/modal/style/index.css';
 import './index.scss';
 
+const MODAL_WIDTH_MAP = {
+  small: 500,
+  medium: 600,
+  large: 800
+};
+
 class Modal extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    width: PropTypes.num
+    type: PropTypes.oneOf(['small', 'medium', 'large'])
+  };
+
+  static defaultProps = {
+    type: 'medium'
   };
 
   render() {
-    let { children, width } = this.props;
+    let { children, type } = this.props;
+    let width = MODAL_WIDTH_MAP[type];
     return (
       <AntModal {...this.props} className="mtModal" width={width}>
         {children}
