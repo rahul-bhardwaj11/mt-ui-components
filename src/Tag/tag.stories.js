197c8608aea255bf926e7f33e77b74d2674131bb
afrozMT <afroz.alam@mindtickle.com>
2018-08-12 20:21:54 +0530

    fixes

--- src/Tag/tag.stories.js
+++ src/Tag/tag.stories.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import Tag from './index';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
-// const CheckableTag = Tag.CheckableTag;
 const stories = storiesOf('Tag', module);
 stories.addDecorator(withKnobs);
 
@@ -49,16 +47,12 @@ stories
   .add(
     'Action tag',
     withInfo('Basic usage of the Default tag action')(() => (
-      <Tag action={true} type="action">
-        {text('children', 'tag')}
-      </Tag>
+      <Tag type="action">{text('children', 'tag')}</Tag>
     ))
   )
   .add(
     'Disabled tag',
     withInfo('Basic usage of the Default tag Disabled')(() => (
-      <Tag disabled={true} type="disabled">
-        {text('children', 'tag')}
-      </Tag>
+      <Tag disabled={true}>{text('children', 'tag')}</Tag>
     ))
   );
