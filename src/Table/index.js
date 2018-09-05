39da72fd9fb2cb38e48e3a121a275b6e8d0df1b2
Tushar Rathi <tushar.rathi@mindtickle.com>
2018-09-05 10:33:27 +0530

    Made css changes in table and badge component

--- src/Table/index.js
+++ src/Table/index.js
@@ -41,6 +41,10 @@ const MtTable = styled.div`
   .ant-table-default > .ant-table-content > .ant-table-scroll > .ant-table-body,
   .ant-table-default
     > .ant-table-content
+    > .ant-table-scroll
+    > .ant-table-header,
+  .ant-table-default
+    > .ant-table-content
     > .ant-table-fixed-left
     > .ant-table-header
     > .ant-table-fixed,
@@ -61,7 +65,6 @@ const MtTable = styled.div`
     > .ant-table-body-outer
     > .ant-table-body-inner
     > .ant-table-fixed {
-    padding: 0px;
     .ant-table-thead {
       & > tr {
         color: ${theme.colors.DARK_OUTER_SPACE};
@@ -71,7 +74,7 @@ const MtTable = styled.div`
           border-bottom: 1px solid ${theme.colors.ALTO};
           padding: ${props => {
             let {
-              contentCellPadding: {
+              headerCellPadding: {
                 pTop,
                 pRight,
                 pBottom,
@@ -83,11 +86,7 @@ const MtTable = styled.div`
           &:last-child {
             padding: ${props => {
               let {
-                contentCellPadding: {
-                  pTop,
-                  pBottom,
-                  pLeft
-                } = DEFAULT_TH_PADDING
+                headerCellPadding: { pTop, pBottom, pLeft } = DEFAULT_TH_PADDING
               } = props;
               return `${pTop} ${pLeft} ${pBottom}  ${pLeft}`;
             }};
@@ -232,16 +231,19 @@ const MtTable = styled.div`
   .ant-table-thead > tr > th.ant-table-selection-column,
   .ant-table-tbody > tr > td.ant-table-selection-column {
     min-width: auto;
-    width: auto;
   }
+
   .ant-checkbox-wrapper {
+    &:hover .ant-checkbox-inner {
+      border-color: ${theme.colors.INDIGO};
+    }
     & .ant-checkbox > .ant-checkbox-inner {
       width: 14px;
       height: 14px;
       border-radius: 3px;
       &:after {
-        left: 3.5px;
-        top: 1.2px;
+        left: 3px;
+        top: 1px;
       }
     }
     & > .ant-checkbox-checked {
@@ -249,20 +251,24 @@ const MtTable = styled.div`
         background-color: ${theme.colors.INDIGO};
         border-color: ${theme.colors.INDIGO};
         &:after {
-          left: 3.5px;
-          top: 1.2px;
+          left: 3px;
+          top: 1px;
         }
       }
     }
     & > .ant-checkbox-indeterminate {
       & > .ant-checkbox-inner {
-        background-color: ${theme.colors.INDIGO};
         border-color: ${theme.colors.INDIGO};
         &:after {
-          left: 1.5px;
-          top: 5px;
+          left: 6px;
+          top: 6px;
+          background-color: ${theme.colors.INDIGO};
         }
       }
+    }
+    .ant-checkbox:hover .ant-checkbox-inner,
+    .ant-checkbox-input:focus + .ant-checkbox-inner {
+      border-color: ${theme.colors.INDIGO};
     }
   }
 `;
