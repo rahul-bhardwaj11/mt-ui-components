ed762a006ae2a5e36089054f61ada9ebd7228ed0
Tushar Rathi <tushar.rathi@mindtickle.com>
2018-09-05 16:54:59 +0530

    Made css changes in card component

--- src/Card/index.js
+++ src/Card/index.js
@@ -3,8 +3,26 @@ import PropTypes from 'prop-types';
 import AntCard from 'antd/lib/card';
 import styled from 'styled-components';
 import 'antd/lib/card/style/index.css';
+import mixins from '../styles/mixins';
 
 const MtCard = styled.div`
+  .ant-card {
+    border-radius: 8px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+    padding: 24px;
+    .ant-card-head {
+      padding: 0 0 12px 0;
+      border-bottom: none;
+      min-height: auto;
+      ${mixins.h3()};
+      .ant-card-head-title {
+        padding: 0px;
+      }
+    }
+    .ant-card-body {
+      padding: 0px;
+    }
+  }
   .ant-card-bordered {
     background: #fff;
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.08);
@@ -12,8 +30,11 @@ const MtCard = styled.div`
     border: transparent;
   }
   .ant-card-wider-padding {
+    .ant-card-head {
+      padding: 0 0 12px 0;
+    }
     .ant-card-body {
-      padding: 24px;
+      padding: 0px;
     }
   }
 `;
@@ -25,7 +46,7 @@ class Card extends Component {
     const { children } = this.props;
     return (
       <MtCard>
-        <AntCard>{children}</AntCard>
+        <AntCard {...this.props}>{children}</AntCard>
       </MtCard>
     );
   }
