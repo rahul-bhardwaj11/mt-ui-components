e07de3d6bb52b7be29142202d52ea5f5d1d974a5
Tushar Rathi <tushar.rathi@mindtickle.com>
2018-09-07 22:45:25 +0530

    Changed datepicker component

--- src/DatePicker/datePicker.stories.js
+++ src/DatePicker/datePicker.stories.js
@@ -11,6 +11,15 @@ stories.addDecorator(withKnobs);
 stories.add(
   'DatePicker',
   withInfo('Adding type primary to button')(() => (
-    <DatePicker fullscreen={false} />
+    <DatePicker
+      fullscreen={false}
+      showToday={false}
+      format={'LL'}
+      placeholder="Select date"
+      allowClear={false}
+      dropdownClassName={'dropdown'}
+      className={'datepicker'}
+      popupStyle={{ width: '100px', height: '100px' }}
+    />
   ))
 );
--- src/DatePicker/index.js
+++ src/DatePicker/index.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-
+// import PropTypes from 'prop-types';
 import AntDatePicker from 'antd/lib/date-picker';
 import 'antd/lib/date-picker/style/index.css';
 import styled from 'styled-components';
+// import theme from '../styles/theme';
 
-const MtDatePicker = styled(AntDatePicker)``;
+const MtDatePicker = styled.div`
+  .ant-calendar-picker-input.ant-input {
+    padding: 6px 24px;
+  }
+  .ant-calendar-picker-icon {
+    display: none;
+  }
+
+  .dropdown {
+    color: blue;
+    border: 1px solid red;
+    .ant-calendar {
+      border: 1px solid red;
+      .ant-calendar-panel {
+        border: 1px solid red;
+        .ant-calendar-input-wrap {
+          border: 1px solid red;
+          display: none;
+          height: 50px;
+        }
+      }
+    }
+  }
+`;
 
 class DatePicker extends Component {
   render() {
-    return <MtDatePicker {...this.props} />;
+    return (
+      <MtDatePicker
+        innerRef={el => {
+          if (el) {
+            this.dropdownRef = el;
+          }
+        }}
+      >
+        <AntDatePicker
+          {...this.props}
+          getCalendarContainer={() => {
+            return this.dropdownRef;
+          }}
+        />
+      </MtDatePicker>
+    );
   }
 }
 
