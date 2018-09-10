ed7746cb58f5e9b8cad171442712f1fe152a73a7
Tushar Rathi <tushar.rathi@mindtickle.com>
2018-09-10 19:05:46 +0530

    Made minor changes in modal component

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import AntModal from 'antd/lib/modal';
-import 'antd/lib/modal/style/index.css';
-import '../styles/override.scss';
-import styled from 'styled-components';
-import theme from '../styles/theme';
-import mixins from '../styles/mixins';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import AntModal from "antd/lib/modal";
+import "antd/lib/modal/style/index.css";
+import "../styles/override.scss";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import mixins from "../styles/mixins";
 
 const MtModal = styled(AntModal)`
   .ant-modal-content {
@@ -74,26 +74,25 @@ const MODAL_WIDTH_MAP = {
   small: 500,
   medium: 600,
   large: 800,
-  full: '100%'
+  full: "100%"
 };
 
 class Modal extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    type: PropTypes.oneOf(['small', 'medium', 'large', 'full'])
+    type: PropTypes.oneOf(["small", "medium", "large", "full"])
   };
 
   render() {
-    let { children, type } = this.props;
+    let { children, type, width } = this.props;
     let customProps = {
       ...this.props,
       width: MODAL_WIDTH_MAP[type],
       style: {
         ...this.props.style,
-        top: type === 'full' ? 0 : undefined
+        top: type === "full" ? 0 : undefined
       }
     };
-    let width = MODAL_WIDTH_MAP[type];
     return (
       <MtModal {...customProps} width={width}>
         {children}
