28ffedd1899ec6b73db9c0adb056fa9db44b1a1e
ekta verma <ekta.verma@mindtickle.com>
2018-09-20 17:59:00 +0530

    done changes for icons

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,22 +1,58 @@
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import classnames from 'classnames';
+
+// const noop = () => undefined;
+// export default class Icon extends Component {
+//   static propTypes = {
+//     type: PropTypes.string.isRequired,
+//     className: PropTypes.string,
+//     onClick: PropTypes.func
+//   };
+//   static defaultProps = {
+//     onClick: noop
+//   };
+//   render() {
+//     const { type, className, onClick } = this.props;
+//     return (
+//       <span
+//         className={classnames(`icon-${type}`, className)}
+//         onClick={onClick}
+//       />
+//     );
+//   }
+// }
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import svgIcons from './svgIcons';
+//import styles from "./index.scss";
 
 const noop = () => undefined;
 export default class Icon extends Component {
   static propTypes = {
     type: PropTypes.string.isRequired,
     className: PropTypes.string,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    gradient: PropTypes.bool
   };
   static defaultProps = {
-    onClick: noop
+    onClick: noop,
+    gradient: false
   };
   render() {
-    const { type, className, onClick } = this.props;
+    const { type, className, onClick, gradient } = this.props;
+    const style = gradient ? { ...style, ...svgIcons[type] } : style;
     return (
       <span
-        className={classnames(`icon-${type}`, className)}
+        className={classnames(
+          {
+            [`icon-${type}`]: !gradient
+          },
+          className
+        )}
+        style={style}
         onClick={onClick}
       />
     );
