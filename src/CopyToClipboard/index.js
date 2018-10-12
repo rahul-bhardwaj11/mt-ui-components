71f5899726a25e207f59e078b86d39669022b833
afrozMT <afroz.alam@mindtickle.com>
2018-10-13 00:13:51 +0530

    jira bug fixed

--- src/CopyToClipboard/index.js
+++ src/CopyToClipboard/index.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 //import classnames from "classnames";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from 'copy-to-clipboard';
 
 export default class CopyToClip extends Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
     onCopy: PropTypes.func,
     children: PropTypes.node
+  };
+
+  static copy = ({ text, options, onCopy }) => {
+    const result = copy(text, options);
+
+    if (onCopy) {
+      onCopy(text, result);
+    }
   };
 
   render() {
