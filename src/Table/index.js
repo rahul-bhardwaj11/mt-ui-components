bdd199247de3d488d740533e3b18f3a9b055fc1a
Krishan Goel <krishangoel@Krishans-MacBook-Pro.local>
2018-10-15 12:17:07 +0530

    table changes

--- src/Table/index.js
+++ src/Table/index.js
@@ -276,7 +276,13 @@ class Table extends Component {
           </ActionBar>
         )}
         {isLoadMore &&
-          hasMore && <Button onClick={this.fetch}>Load More</Button>}
+          hasMore && (
+            <div className="loadMoreBtnDiv">
+              <Button type="secondary" size="medium" onClick={this.fetch}>
+                Load More
+              </Button>
+            </div>
+          )}
       </MtTable>
     );
   }
