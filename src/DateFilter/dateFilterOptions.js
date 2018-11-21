252ac6d3b9479733a4d7ee4b9efb645a2a207ee2
Ramakrishna <ramakrishna@mindtickle.com>
2018-11-21 16:30:26 +0530

    added default value in date filter and optimisations

--- src/DateFilter/dateFilterOptions.js
+++ src/DateFilter/dateFilterOptions.js
@@ -1,67 +1,62 @@
 import moment from 'moment';
 const RANGE_PICKER_STATE = 'range-picker';
 
+export const defaultFormatter = (from, to) =>
+  `${from.format("D MMM' YY")} ~ ${to.format("D MMM' YY")}`;
+
+const yesterday = moment().subtract(1, 'days');
+
+const lastWeek = moment().subtract(1, 'week');
+const lastWeekFrom = moment(lastWeek).startOf('isoweek');
+const lastWeekTo = moment(lastWeek).endOf('isoweek');
+
+const lastMonth = moment().subtract(1, 'month');
+
+const lastQuarter = moment().subtract(1, 'Q');
+const lastQuarterFrom = moment(lastQuarter).startOf('Q');
+const lastQuarterTo = moment(lastQuarter).endOf('Q');
+
 const DATE_FILTER_OPTIONS = {
   DAY: {
     key: '0',
     content: 'Yesterday',
-    resolver: () => {
-      const yesterday = moment().subtract(1, 'days');
-      const from = moment(yesterday).startOf('day');
-      const to = moment(yesterday).endOf('day');
-      return {
-        from,
-        to,
-        display: yesterday.format("D MMMM' YY")
-      };
+    resolver: {
+      from: moment(yesterday).startOf('day'),
+      to: moment(yesterday).endOf('day'),
+      display: yesterday.format("D MMMM' YY")
     }
   },
   WEEK: {
     key: '1',
     content: 'Last Week',
-    resolver: () => {
-      const lastWeek = moment().subtract(1, 'week');
-      const from = moment(lastWeek).startOf('isoweek');
-      const to = moment(lastWeek).endOf('isoweek');
-      return {
-        from,
-        to,
-        display: `${from.format("D MMM' YY")} ~ ${to.format("D MMM' YY")}`
-      };
+    resolver: {
+      from: lastWeekFrom,
+      to: lastWeekTo,
+      display: defaultFormatter(lastWeekFrom, lastWeekTo)
     }
   },
   MONTH: {
     key: '2',
     content: 'Last Month',
-    resolver: () => {
-      const lastMonth = moment().subtract(1, 'month');
-      const from = moment(lastMonth).startOf('month');
-      const to = moment(lastMonth).endOf('month');
-      return {
-        from,
-        to,
-        display: `${lastMonth.format('MMMM, YY')}`
-      };
+    resolver: {
+      from: moment(lastMonth).startOf('month'),
+      to: moment(lastMonth).endOf('month'),
+      display: `${lastMonth.format('MMMM, YY')}`
     }
   },
   QUARTER: {
     key: '3',
     content: 'Last Quarter',
-    resolver: () => {
-      const lastQuarter = moment().subtract(1, 'Q');
-      const from = moment(lastQuarter).startOf('Q');
-      const to = moment(lastQuarter).endOf('Q');
-      return {
-        from,
-        to,
-        display: `${from.format("MMM' YY")} ~ ${to.format("MMM' YY")}`
-      };
+    resolver: {
+      from: lastQuarterFrom,
+      to: lastQuarterTo,
+      display: defaultFormatter(lastQuarterFrom, lastQuarterTo)
     }
   },
   CUSTOM: {
     key: '4',
     content: 'Custom Range',
-    resolver: () => RANGE_PICKER_STATE
+    resolver: RANGE_PICKER_STATE
   }
 };
 
