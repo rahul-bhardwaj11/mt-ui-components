a144e97fee306f6fc9d273a034832d3ae038021e
Tushar Rathi <tushar.rathi@mindtickle.com>
2018-11-29 17:38:53 +0530

    Autocomplete component chaanges

--- src/AutoComplete/index.js
+++ src/AutoComplete/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import AntAutoComplete from 'antd/lib/auto-complete';
+import PropTypes from 'prop-types';
 import Styled from 'styled-components';
 import 'antd/lib/auto-complete/style/index.css';
 
 const StyledAutoComplete = Styled.div`
+  display: inline-block;
 `;
 
 class AutoComplete extends React.Component {
+  static propTypes = {
+    className: PropTypes.string
+  };
   render() {
+    const { className, ...rest } = this.props;
     return (
       <StyledAutoComplete
         innerRef={ele => {
           if (ele) this.popUpContainer = ele;
         }}
+        className={className}
       >
         <AntAutoComplete
           getPopupContainer={() => this.popUpContainer}
-          {...this.props}
+          {...rest}
         />
       </StyledAutoComplete>
     );
   }
 }
+
 AutoComplete.Option = AntAutoComplete.Option;
 export default AutoComplete;
