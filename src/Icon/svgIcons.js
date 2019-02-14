465371d2a56cf4171c5f0fa4ddcc354af8f38c3f
nitesh5692 <nitesh.bhandari@mindtickle.com>
2019-02-14 11:23:04 +0530

    new images added and backmerged from prod

--- src/Icon/svgIcons.js
+++ src/Icon/svgIcons.js
@@ -4,6 +4,9 @@ import keywordsFull from './assests/images/keywordsFull.svg';
 import textConditionFull from './assests/images/textConditionFull.svg';
 import keywordsFullActive from './assests/images/keywordsFullActive.svg';
 import textConditionFullActive from './assests/images/textConditionFullActive.svg';
+import internalError from './assests/images/internalError.svg';
+import pageNotFound from './assests/images/pageNotFound.svg';
+import serviceUnavailable from './assests/images/serviceUnavailable.svg';
 
 export default {
   outlookCalendar: ({ width = 30, height = 30 }) => ({
@@ -33,6 +36,21 @@ export default {
   }),
   textConditionFullActive: ({ width = 30, height = 30 }) => ({
     background: `url(${textConditionFullActive})  no-repeat 0 0 / contain transparent`,
+    width: width,
+    height: height
+  }),
+  internalError: ({ width = 100, height = 100 }) => ({
+    background: `url(${internalError})  no-repeat 0 0 / contain transparent`,
+    width: width,
+    height: height
+  }),
+  pageNotFound: ({ width = 100, height = 100 }) => ({
+    background: `url(${pageNotFound})  no-repeat 0 0 / contain transparent`,
+    width: width,
+    height: height
+  }),
+  serviceUnavailable: ({ width = 100, height = 100 }) => ({
+    background: `url(${serviceUnavailable})  no-repeat 0 0 / contain transparent`,
     width: width,
     height: height
   })
