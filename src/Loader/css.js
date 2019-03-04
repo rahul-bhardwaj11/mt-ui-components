988453726fff1292e2efe2597cd76d1289df74d6
afrozMT <afroz.alam@mindtickle.com>
2019-03-04 12:18:25 +0530

    loader fix

--- src/Loader/css.js
+++ src/Loader/css.js
@@ -75,7 +75,9 @@ const StyledLoader = styled.div`
       border: 6px solid #fff;
       border-radius: 50%;
       animation: spinner 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-      border-color: ${({ loaderStyle: { borderColor } }) => borderColor}
+      border-color: ${({ loaderStyle: { borderColor } = {} }) => {
+          return borderColor;
+        }}
         transparent transparent transparent;
       &:nth-child(1) {
         animation-delay: -0.45s;
