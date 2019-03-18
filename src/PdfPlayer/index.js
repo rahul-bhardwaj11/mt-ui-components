9cfe199a058da851406a618472aadc382e0c2a9e
krishangoel21 <krishan@zepo.biz>
2019-03-18 15:49:12 +0530

    conflict resolve

--- src/PdfPlayer/index.js
+++ src/PdfPlayer/index.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import Input from '../Input';
 import ConfirmBox from '../ConfirmBox';
 import Tag from '../Tag';
+import Icon from '../Icon';
+import MTPDFPlayer from './style';
 
 class PdfPlayer extends Component {
   static propTypes = {
     nextPage: PropTypes.number,
-    src: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
     uuid: PropTypes.string.isRequired,
     onPageChange: PropTypes.func,
     isEditMode: PropTypes.bool,
@@ -18,7 +20,7 @@ class PdfPlayer extends Component {
   };
 
   static defaultProps = {
-    style: { width: 650, height: 378 }
+    style: { width: 620, height: 350 }
   };
 
   state = {
@@ -27,7 +29,7 @@ class PdfPlayer extends Component {
   };
 
   componentDidMount() {
-    const { src, uuid } = this.props;
+    const { url, uuid } = this.props;
     let staticUrl = uuid + '&width=547&height=401';
     staticUrl = staticUrl.replace('crocodoc', 'pdfViewer');
 
@@ -40,18 +42,16 @@ class PdfPlayer extends Component {
     let input = document.createElement('input');
     input.type = 'hidden';
     input.name = 'pdf';
-    input.value = src;
+    input.value = url;
     form.appendChild(input);
 
     let iframe = document.createElement('iframe');
     this.iframe = iframe;
     iframe.name = uuid;
     iframe.scrolling = 'no';
-    iframe.style.position = 'absolute';
-    iframe.style.left = '0px';
-    iframe.style.border = '0px';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
+    iframe.style.border = '0';
 
     this.ref.appendChild(iframe);
     this.ref.appendChild(form);
@@ -108,19 +108,36 @@ class PdfPlayer extends Component {
 
   renderEditTitleDiv = () => {
     const { edit, title } = this.state;
-    return edit ? (
-      <div>
-        <Input
-          defaultValue={title}
-          onChange={(e, value) => {
-            this.newTitle = value;
-          }}
-        />
-        <Tag onClick={this.handleTitleChange}>save</Tag>
-        <Tag onClick={() => this.setState({ edit: false })}>cancel</Tag>
+    return (
+      <div className="editMode">
+        {edit ? (
+          <React.Fragment>
+            <div className="inputBox">
+              <Input
+                className="inputTitle"
+                defaultValue={title}
+                onChange={(e, value) => {
+                  this.newTitle = value;
+                }}
+              />
+            </div>
+            <div className="tagBox">
+              <Tag
+                className="saveTag"
+                type="action"
+                onClick={this.handleTitleChange}
+              >
+                Save
+              </Tag>
+              <Tag onClick={() => this.setState({ edit: false })}>Cancel</Tag>
+            </div>
+          </React.Fragment>
+        ) : (
+          <div className="titleText" onClick={this.activeEditMode}>
+            {title}
+          </div>
+        )}
       </div>
-    ) : (
-      <div onClick={this.activeEditMode}>{title}</div>
     );
   };
 
@@ -132,7 +149,7 @@ class PdfPlayer extends Component {
           onRemove && onRemove();
         }}
       >
-        Replace
+        <Icon type="delete2" className="replaceModeIcon" />
       </ConfirmBox>
     );
   };
@@ -140,18 +157,15 @@ class PdfPlayer extends Component {
   render() {
     const { style, isEditMode } = this.props;
     return (
-      <React.Fragment>
+      <MTPDFPlayer>
         {isEditMode && (
-          <div>
+          <div className="uploaderHeader">
             {this.renderEditTitleDiv()}
             {this.renderReplaceDiv()}
           </div>
         )}
-        <div
-          ref={e => (this.ref = e)}
-          style={{ minHeight: 'inherit', position: 'relative', ...style }}
-        />
-      </React.Fragment>
+        <div ref={e => (this.ref = e)} style={style} />
+      </MTPDFPlayer>
     );
   }
 }
