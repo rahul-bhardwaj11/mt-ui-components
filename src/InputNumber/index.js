0bd0bda63a09940ae0a925b35f13096970d0717c
krishangoel21 <krishan@zepo.biz>
2019-03-26 15:34:51 +0530

    inputnumber css changes

--- src/InputNumber/index.js
+++ src/InputNumber/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import AntInputNumber from 'antd/lib/input-number';
 import 'antd/lib/input-number/style/index.css';
+import styled from 'styled-components';
+
+const MTInputNumber = styled(AntInputNumber)`
+  .ant-input-number-handler-up-inner:before {
+    content: '';
+  }
+  &.ant-input-number-focused {
+    border-color: transparent !important;
+    box-shadow: none !important;
+  }
+`;
+
 class InputNumber extends Component {
   render() {
-    return <AntInputNumber {...this.props} />;
+    return <MTInputNumber {...this.props} autoFocus={false} />;
   }
 }
 
