f908c1e0084000024ecf4f2e72a210e0930c8299
krishangoel21 <krishan@zepo.biz>
2019-03-26 16:03:45 +0530

    correction in input number

--- src/InputNumber/index.js
+++ src/InputNumber/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import AntInputNumber from 'antd/lib/input-number';
 import 'antd/lib/input-number/style/index.css';
 import styled from 'styled-components';
+import theme from '../styles/theme';
 
 const MTInputNumber = styled(AntInputNumber)`
   .ant-input-number-handler-up-inner:before {
     content: '';
   }
   &.ant-input-number-focused {
-    border-color: transparent !important;
-    box-shadow: none !important;
+    border-color: ${theme.colors.SILVER} !important;
+    box-shadow: none;
+  }
+  &.ant-input-number:hover {
+    border-color: ${theme.colors.SILVER};
   }
 `;
 
