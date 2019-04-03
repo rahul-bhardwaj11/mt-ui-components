3ba98abb168429d3f2b0fcb3f1c447da66153f26
nitesh5692 <nitesh.bhandari@mindtickle.com>
2019-04-03 10:48:40 +0530

    Error Page: go to home button added + Structural changes

---
+++ src/ErrorPage/constants.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import cs from 'classnames';
+import Button from '../Button';
+
+export const PAGE_TYPES = {
+  NOT_FOUND: '404',
+  INTERNAL_SERVER_ERROR: '500',
+  SERVICE_UNAVAILABLE: '503',
+  CLIENT_ERROR: 'CLIENT_ERROR'
+};
+/*eslint-disable react/display-name */
+const BUTTON_TYPES = {
+  GO_TO_HOME: () => (
+    <Button
+      type="secondary"
+      className="errorCommonButtonStyling"
+      onClick={() => (location.href = '/')}
+    >
+      Go to Home
+    </Button>
+  ),
+  REPORT_ISSUE: () => (
+    <Button type="secondary" className="errorCommonButtonStyling">
+      Report Issue
+    </Button>
+  ),
+  TRY_AGAIN: () => (
+    <Button
+      type="primary"
+      className="errorCommonButtonStyling"
+      onClick={() => location.reload()}
+    >
+      Try Again
+    </Button>
+  )
+};
+
+export const PAGE_TYPES_CONFIG = {
+  [PAGE_TYPES.NOT_FOUND]: {
+    image: 'pageNotFound',
+    heading: 'Page not found',
+    description: "We can't seem ot find the page you are looking for.",
+    buttons: styleActionButton => (
+      <div className={cs('errorPageActionButtons', styleActionButton)}>
+        <BUTTON_TYPES.GO_TO_HOME />
+      </div>
+    )
+  },
+  [PAGE_TYPES.INTERNAL_SERVER_ERROR]: {
+    image: 'internalError',
+    heading: 'Something went wrong',
+    description:
+      "Sorry your past request could't be completed <br /> Please try agaain after some time",
+    buttons: styleActionButton => (
+      <div className={cs('errorPageActionButtons', styleActionButton)}>
+        <BUTTON_TYPES.GO_TO_HOME />
+        <BUTTON_TYPES.TRY_AGAIN />
+      </div>
+    )
+  },
+  [PAGE_TYPES.SERVICE_UNAVAILABLE]: {
+    image: 'serviceUnavailable',
+    heading: "It's not you. It's us",
+    description:
+      'Sorry our service is not available currently. <br />  Please try again after some time',
+    buttons: styleActionButton => (
+      <div className={cs('errorPageActionButtons', styleActionButton)}>
+        <BUTTON_TYPES.GO_TO_HOME />
+        <BUTTON_TYPES.TRY_AGAIN />
+      </div>
+    )
+  },
+  [PAGE_TYPES.CLIENT_ERROR]: {
+    image: 'serviceUnavailable',
+    heading: 'Something went wrong',
+    description:
+      "Sorry your past request could't be completed <br /> Please try agaain after some time",
+    buttons: (styleActionButton, reportError) => (
+      <div className={cs('errorPageActionButtons', styleActionButton)}>
+        <Button type="secondary" className="errorCommonButtonStyling">
+          Reload
+        </Button>
+        {reportError && (
+          <Button
+            type="primary"
+            className="errorCommonButtonStyling"
+            onClick={reportError}
+          >
+            Report Error
+          </Button>
+        )}
+      </div>
+    )
+  }
+};
+/*eslint-disable react/display-name */
