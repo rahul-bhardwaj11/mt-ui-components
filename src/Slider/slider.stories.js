e894ebbe0e76eb25f14b5a6c28683c0f271b6e98
krishangoel21 <krishan@zepo.biz>
2019-04-26 13:13:03 +0530

    few stories changes

--- src/Slider/slider.stories.js
+++ src/Slider/slider.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Slider from '.';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 
 function formatter(value) {
   return `${value}%`;
@@ -31,55 +31,36 @@ const stories = storiesOf('Slider', module);
 stories.addDecorator(withKnobs);
 
 stories.add(
-  'Slider Empty',
+  'Slider with dots',
   withInfo('Slider without tooltip')(() => (
-    <Slider step={2} min={0} max={10} value={null} marks={marks} />
-  ))
-);
-stories.add(
-  'Slider filled with marker',
-  withInfo('Slider with  tooltip on every step')(() => (
     <Slider
+      min={0}
+      max={10}
+      marks={marks}
+      defaultValue={6}
+      tipFormatter={formatter}
+      disabled={boolean('disabled', false)}
       dots={true}
       step={2}
-      min={0}
-      max={10}
-      marks={marks}
-      defaultValue={6}
     />
   ))
 );
 stories.add(
-  'Slider disabled filled without marker',
-  withInfo('Slider with tooltip')(() => (
-    <Slider tipFormatter={formatter} disabled value={30} />
-  ))
-);
-
-stories.add(
-  'Slider disabled without marker',
-  withInfo('Slider with tooltip ')(() => (
-    <Slider tipFormatter={formatter} disabled />
-  ))
-);
-
-stories.add(
-  'Slider Disabled  with marker',
-  withInfo('Slider with  tooltip on every step')(() => (
-    <Slider dots={true} step={1} min={0} max={10} marks={marks} disabled />
-  ))
-);
-stories.add(
-  'Slider Disabled Filled with marker',
-  withInfo('Slider with  tooltip on every step')(() => (
+  'Slider without dots',
+  withInfo('Slider without tooltip')(() => (
     <Slider
-      dots={true}
-      step={1}
       min={0}
       max={10}
       marks={marks}
-      disabled
       defaultValue={6}
+      tipFormatter={formatter}
+      disabled={boolean('disabled', false)}
     />
+  ))
+);
+stories.add(
+  'Slider with only value',
+  withInfo('Slider without tooltip')(() => (
+    <Slider tipFormatter={formatter} disabled value={30} />
   ))
 );
